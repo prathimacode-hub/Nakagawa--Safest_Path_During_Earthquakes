758f43b0a51c794abad7b9aa54849f545709b7e6
Prathima Kadari <kadariprathima4@gmail.com>
2022-06-06 16:32:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -199,12 +199,15 @@ elif add_selectbox == 'Safest Path':
         
         G = ox.add_edge_speeds(G) #Impute
         G = ox.add_edge_travel_times(G) #Travel time
-
-        # start = (57.715495, 12.004210)
-        # end = (57.707166, 11.978388)
 	
-	start = (35.1547177, 136.826244)
-	end = (35.1394307, 136.8565519)
+	# start = (57.715495, 12.004210)
+        # end = (57.707166, 11.978388)
+
+        #start = (35.1547177, 136.826244)
+	#end = (35.1394307, 136.8565519)
+	start = (35.1433217, 136.8425095)
+	end = (35.1301611, 136.8697751)
+        #end = (35.1301611, 136.8697751)
 	
 	start_node = ox.get_nearest_node(G, start)
         end_node = ox.get_nearest_node(G, end)# Calculate the shortest path
